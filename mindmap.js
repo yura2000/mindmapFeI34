29bd0011f0680c37b5e9c66db13e043f2cda77f9
Oleh Kosylo <kosylopoker@gmail.com>
2020-05-24 19:59:03 +0300

    Update map
    Update map

--- mindmap.js
+++ mindmap.js
@@ -38,4 +38,31 @@ var API_4_MINDMAP = function() {
         };
         return arguments.callee.instance;
     }
+
+
+    //Update map
+    this.jsRefreshMindmap = function() { // швидке оновлення всієї карти на екрані зі збереженням стану
+        myjsPlumb.reset();// стираємо все лінії
+        var save_scroll_top = $("#mindmap").scrollTop();// зберігаємо позиції скролінгу, щоб повернути
+        var save_scroll_left = $("#mindmap").scrollLeft();//всё как было после перереисовки
+
+        var hidden_elements = [];// масив зберігання згорнутих елементів
+
+        $(".hide").each(function(){
+            hidden_elements.push($(this).attr("myid"));
+        });
+
+        api4mindmap.jsRenderAllMap(1);// перемальовували всю карту заново
+
+        $.each(hidden_elements, function(i, el){ // приховуємо елементи, які були приховані до.
+            $("#node_"+el).addClass("hide");
+        });
+
+        api4mindmap.jsDrawMindmap(1);// намічаємо лінії, взявши видимі вузли з екрану
+        onResize();// запускаємо отрисовку закеширувалася ліній
+
+        $("#mindmap").scrollTop(save_scroll_top); // зберігаємо позиції скролінгу, щоб повернути
+        $("#mindmap").scrollLeft(save_scroll_left);// все як було після перемальовки
+
+    }
 };
