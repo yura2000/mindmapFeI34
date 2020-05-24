a68bc27c94239299ce61cf087e1ab5085357c962
Oleh Kosylo <kosylopoker@gmail.com>
2020-05-24 20:13:59 +0300

    Update map
    update map

--- mindmap.js
+++ mindmap.js
@@ -47,6 +47,32 @@ var API_4_MINDMAP = function() {
 
         };
         return arguments.callee.instance;
+
+        this.jsRefreshMindmap = function() {
+            myjsPlumb.reset();
+            var save_scroll_top = $("#mindmap").scrollTop();
+            var save_scroll_left = $("#mindmap").scrollLeft();
+
+            var hidden_elements = [];
+
+            $(".hide").each(function(){
+                hidden_elements.push($(this).attr("myid"));
+            });
+
+            api4mindmap.jsRenderAllMap(1);
+
+            $.each(hidden_elements, function(i, el){
+                $("#node_"+el).addClass("hide");
+            });
+
+            api4mindmap.jsDrawMindmap(1);
+            onResize();
+
+            $("#mindmap").scrollTop(save_scroll_top);
+            $("#mindmap").scrollLeft(save_scroll_left);
+
+        }
+
     }
 	
 	
@@ -72,31 +98,9 @@ var API_4_MINDMAP = function() {
 		 	 }
 
 
-    //Update map
-    this.jsRefreshMindmap = function() { // швидке оновлення всієї карти на екрані зі збереженням стану
-        myjsPlumb.reset();// стираємо все лінії
-        var save_scroll_top = $("#mindmap").scrollTop();// зберігаємо позиції скролінгу, щоб повернути
-        var save_scroll_left = $("#mindmap").scrollLeft();//всё как было после перереисовки
 
-        var hidden_elements = [];// масив зберігання згорнутих елементів
 
-        $(".hide").each(function(){
-            hidden_elements.push($(this).attr("myid"));
-        });
 
-        api4mindmap.jsRenderAllMap(1);// перемальовували всю карту заново
-
-        $.each(hidden_elements, function(i, el){ // приховуємо елементи, які були приховані до.
-            $("#node_"+el).addClass("hide");
-        });
-
-        api4mindmap.jsDrawMindmap(1);// намічаємо лінії, взявши видимі вузли з екрану
-        onResize();// запускаємо отрисовку закеширувалася ліній
-
-        $("#mindmap").scrollTop(save_scroll_top); // зберігаємо позиції скролінгу, щоб повернути
-        $("#mindmap").scrollLeft(save_scroll_left);// все як було після перемальовки
-
-    }
 };
 
 var myjsPlumb;
